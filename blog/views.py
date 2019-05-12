401cd7877cbb2f21094e41b403fb0a3a595df505
Pablo CABRERA <pablo.cabrera@alten.es>
2019-05-12 18:15:33 +0200

    Added new html page for the post

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import datetime
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 # vistas que cargaran nuestras paginas html
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
